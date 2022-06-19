bb704403e85a88447ad55b76e65e99f3589b8327
David Ryan <3d.ryanm@gmail.com>
2022-06-18 23:24:30 -0600

    Performance improvement

--- gameOfLife.js
+++ gameOfLife.js
@@ -3,6 +3,8 @@ let boardHeight = 50
 let play = false
 let mouseDown = false
 let cellColor = ""
+let aliveCells = []
+let deadCells = []
 
 function createBoard() {
     let width = parseInt(document.getElementById("boardWidthInput").value);
@@ -19,6 +21,7 @@ function createBoard() {
             cell.setAttribute("onmousedown", "boardMouseDown(this)")
             cell.setAttribute("onmouseover", "cellMousedOver(this)")
             cell.setAttribute("onmouseup", "boardMouseUp()")
+            cell.setAttribute("position", i*width+j)
             if(document.getElementById("cellBorderInput").checked) {
                 cell.style.borderStyle = "solid"
                 cell.style.borderWidth = "1px"
@@ -70,7 +73,8 @@ function boardMouseUp(){
 
 function cellMousedOver(element) {
     if(mouseDown) {
-        element.style.backgroundColor = cellColor
+        let position = element.getAttribute("position")
+        aliveCells.push(position)
     }
 }
 
@@ -87,10 +91,13 @@ function fillBoardRandomly() {
 }
 
 function fillBoard(cellStates) {
+    aliveCells = []
+    deadCells = []
     let cells = getCells()
     for (let i = 0; i < cells.length; i++) {
         if (cellStates[i]) {
             createCell(cells, i)
+            aliveCells.push(i)
         } else {
             killCell(cells, i)
         }
@@ -138,16 +145,33 @@ function stop() {
 function nextGeneration() {
     let cells = getCells()
     let nextGeneration = []
-    for (let i = 0; i < cells.length; i++) {
-        let aliveNeighbors = getAliveNeighbors(cells, i)
-        if(aliveNeighbors === 3 || (aliveNeighbors === 2 && isCellAlive(cells, i))){
+    for(let i = 0; i < aliveCells.length; i++) {
+        let aliveNeighbors = getAliveNeighbors(cells, aliveCells[i])
+        if(aliveNeighbors === 3 || aliveNeighbors === 2) {
+            nextGeneration[aliveCells[i]] = true
+        }
+    }
+    for(let i = 0; i < deadCells.length; i++) {
+        if(deadCells[i] === 3){
             nextGeneration[i] = true
-        } else {
-            nextGeneration[i] = false
         }
     }
     fillBoard(nextGeneration)
 }
+
+// function nextGeneration() {
+//     let cells = getCells()
+//     let nextGeneration = []
+//     for (let i = 0; i < cells.length; i++) {
+//         let aliveNeighbors = getAliveNeighbors(cells, i)
+//         if(aliveNeighbors === 3 || (aliveNeighbors === 2 && isCellAlive(cells, i))){
+//             nextGeneration[i] = true
+//         } else {
+//             nextGeneration[i] = false
+//         }
+//     }
+//     fillBoard(nextGeneration)
+// }
 
 function getAliveNeighbors(cells, position) {
     let leftEdge = false
@@ -168,7 +192,7 @@ function getAliveNeighbors(cells, position) {
         position + boardWidth,
         position + boardWidth + 1
     ]
-    
+
     let aliveNeighbors = 0;
     for (let n = 0; n < possibleNeighbors.length; n++) {
         let neighbor = possibleNeighbors[n]
@@ -183,10 +207,55 @@ function getAliveNeighbors(cells, position) {
         }
         if(isCellAlive(cells, neighbor)) {
             aliveNeighbors++
+        } else {
+            if(deadCells[neighbor] !== undefined) {
+                deadCells[neighbor]++
+            } else {
+                deadCells[neighbor] = 1
+            }
         }
     }
     return aliveNeighbors
 }
+
+// function getAliveNeighbors(cells, position) {
+//     let leftEdge = false
+//     let rightEdge = false
+//     let remainder = position % boardWidth
+//     if (remainder === 0) {
+//         leftEdge = true
+//     } else if (remainder === boardWidth - 1) {
+//         rightEdge = true
+//     }
+//
+//     let possibleNeighbors = [
+//         position - boardWidth - 1,
+//         position - boardWidth,
+//         position - (boardWidth - 1),
+//         position - 1, position + 1,
+//         position + (boardWidth - 1),
+//         position + boardWidth,
+//         position + boardWidth + 1
+//     ]
+//
+//     let aliveNeighbors = 0;
+//     for (let n = 0; n < possibleNeighbors.length; n++) {
+//         let neighbor = possibleNeighbors[n]
+//         if (neighbor < 0 || neighbor >= boardWidth * boardHeight) {
+//             continue
+//         }
+//         if (leftEdge && neighbor % boardWidth === boardWidth - 1) {
+//             continue
+//         }
+//         if (rightEdge && neighbor % boardWidth === 0) {
+//             continue
+//         }
+//         if(isCellAlive(cells, neighbor)) {
+//             aliveNeighbors++
+//         }
+//     }
+//     return aliveNeighbors
+// }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
